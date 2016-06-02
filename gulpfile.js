73f7c825936c4736d3f01e214c96476bd81f40d8
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2016-06-02 15:06:54 +0100

    Use gulp and browserify. Setup for client tests. Project structure based on http://fluxusfrequency.github.io/blog/2015/04/01/integrating-react-with-backbone/

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// Gulp Dependencies
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+// Build Dependencies
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+
+// Style Dependencies
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+// Development Dependencies
+var jshint = require('gulp-jshint');
+
+// Test Dependencies
+var mochaPhantomjs = require('gulp-mocha-phantomjs');
+
+gulp.task('lint-client', function() {
+  return gulp.src('./src/client/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src('./src/test/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+
+gulp.task('browserify-client', ['lint-client'], function() {
+  return gulp.src('src/client/index.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('garminello.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('browserify-test', ['lint-test'], function() {
+  return gulp.src('src/test/client/index.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('client-test.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('test', ['lint-test', 'browserify-test'], function() {
+  return gulp.src('src/test/client/index.html')
+    .pipe(mochaPhantomjs());
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/client/less/index.less')
+    .pipe(less())
+    .pipe(prefix({ cascade: true }))
+    .pipe(rename('garminello.css'))
+    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('minify', ['styles'], function() {
+  return gulp.src('build/garminello.css')
+    .pipe(minifyCSS())
+    .pipe(rename('garminello.min.css'))
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('uglify', ['browserify-client'], function() {
+  return gulp.src('build/garminello.js')
+    .pipe(uglify())
+    .pipe(rename('garminello.min.js'))
+    .pipe(gulp.dest('public/javascripts'));
+});
+
+gulp.task('build', ['uglify', 'minify']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/client/**/*.js', ['build', 'test']);
+  gulp.watch('src/test/client/**/*.js', ['test']);
+  gulp.watch('src/client/less/*.less', ['minify']);
+});
+
+gulp.task('default', ['build', 'test', 'watch']);
